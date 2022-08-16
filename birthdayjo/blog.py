372832c92754b0f6cfd9c2ccc6b27544e8bcc726
musrex <96090997+musrex@users.noreply.github.com>
2022-08-16 16:57:50 -0400

    Back to blog, need image upload

--- birthdayjo/blog.py
+++ birthdayjo/blog.py
@@ -1,13 +1,13 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for, send_from_directory)
-
+    Flask, Blueprint, flash, g, redirect, render_template, request, url_for, send_from_directory)
 from werkzeug.exceptions import abort
 from werkzeug.utils import secure_filename
 from birthdayjo.auth import login_required
 from birthdayjo.db import get_db
 
 import os
-UPLOAD_FOLDER = 'static/img',
+
+UPLOAD_FOLDER = '/static/img',
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 bp = Blueprint('blog', __name__, url_prefix='/gallery')
 
@@ -36,13 +36,13 @@ def allowed_file(filename):
 
 @bp.route('/static/img/<filename>')
 def upload(filename):
-    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+    return send_from_directory(['UPLOAD_FOLDER'], filename)
 
 @bp.route('/blog')
 def gallery():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username, filename'
+        'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -54,34 +54,22 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
-        file = request.files['upload']
         error = None
         
         if not title:
             error = 'Title is required.'
-       
-        #if 'file' not in request.files:
-         #   error = 'Error 1: No file selected.'
-
-        if file.filename == '':
-             flash('Error 2: No file selected.')
 
         if error is not None:
             flash(error)
         else:
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(['UPLOAD_FOLDER'], filename))
-                file = str(filename)
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id, file)'
-                ' VALUES (?, ?, ?, ?)',
-                (title, body, g.user['id'], file)
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
             )
             db.commit()
             return redirect(url_for('blog.gallery'))
-
     return render_template('blog/create.html')
 
 
